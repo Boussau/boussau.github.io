b6a999161f741d3cf4a51e3be702f227b14c6cb8
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-10-06 14:41:15 +0200

    trying my luck...

--- deploy.sh
+++ deploy.sh
@@ -29,8 +29,8 @@ then
 
     # build the site
     cd _site
-    git fetch --quiet origin
-    git reset --quiet --hard origin/master
+    #git fetch --quiet origin
+    #git reset --quiet --hard origin/master
     cd ..
     if ! bundle exec jekyll build --trace; then
         echo "Jekyll build failed. Master not updated."
